181ec3684bbace8c6df16b192517674e4517932c
Sanaz Soltani <74157401+Snzslt@users.noreply.github.com>
2024-01-14 17:49:11 -0500

    splitting Components Up to solve problems

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,23 +1,48 @@
 import { useState, useCallback } from 'react';
 import QUESTIONS from '../questions.js';
 import quizCompleteImg from '../assets/quiz-complete.png';
-import QuestionTimer from './QuestionTimer.jsx';
+import Question from './Question.jsx';
 
 export default function Quiz(){
-    
-    const [userAnswers, setUserAnswer] = useState([]);
-    const activeQuestionIndex = userAnswers.length;
    
+    const [answerState, setAnswerState] = useState('');
+    const [userAnswers, setUserAnswer] = useState([]);
+    const activeQuestionIndex = answerState === '' ? userAnswers.length : userAnswers.length - 1;
     const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+    console.log(QUESTIONS.length);
+    console.log(activeQuestionIndex);
+  
 
-   const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer){
+   const handleSelectAnswer = useCallback(function handleSelectAnswer(
+       selectedAnswer
+       ){
+        setAnswerState('answered');
         setUserAnswer((prevUserAnswers) => {
+
         return [...prevUserAnswers, selectedAnswer];
         });
+        setTimeout(()=> {
+            if(selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]){
+                setAnswerState('correct');
+            }
+            else{
+                setAnswerState('wrong');
+            }
 
-    },[]);
-    const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
-    
+            setTimeout(() => {
+                setAnswerState('');
+            }, 2000);
+
+        },1000);
+
+    },[activeQuestionIndex]);
+
+
+    const handleSkipAnswer = useCallback(
+        () => handleSelectAnswer(null),
+         [handleSelectAnswer]
+         );
+
     if (quizIsComplete){
         return <div id='summary'>
             <img src={quizCompleteImg} alt="Trophy icon"/>
@@ -25,25 +50,21 @@ export default function Quiz(){
             </div>
     }
 
-    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-    shuffledAnswers.sort(()=> Math.random() - 0.5);
-
-
+     /*as we have two same key in here we can check an condition and make the two components not to be sibling any more or we can
+            add new compnent and put all the code in there and use one key property for them all
+            // I chosed to add condition instead of new component vut you can refer to 242 video */
+         
     return (
-    <div id='quiz'>
-        <div id='question'>
-            <QuestionTimer
-             timeout={10000}
-             onTimeout={() => handleSelectedAnswer(null)}
-             />
-        <p>{QUESTIONS[activeQuestionIndex].text}</p>
-        <ul id='answers'>
-            {shuffledAnswers.map((answer) => (
-            <li key ={answer} className='answer'>
-                <button onClick={()=> handleSelectedAnswer(answer)}>{answer}</button>
-            </li>))}
-        </ul>
+        <div id='quiz'>
+          <Question 
+          key = {activeQuestionIndex}
+          questionText={QUESTIONS[activeQuestionIndex].text}
+          answers={QUESTIONS[activeQuestionIndex].answers}
+          answerState = {answerState}
+          selectedAnswer ={userAnswers[userAnswers.length - 1]}
+          onSelectAnswer = {handleSelectAnswer}
+          onSkipAnswer = {handleSkipAnswer}/>
         </div>
-        </div>
-        );
+      );
+      
 }
